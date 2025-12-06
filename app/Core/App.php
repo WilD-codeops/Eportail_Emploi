83b2e4e2ba654bdbcb6ac6b7e0103c88a0be7645
WilD <wild.ajc1@gmail.com>
2025-12-06 14:33:26 +0100

    feat(core): création app.php

---
+++ app/Core/App.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+use App\Core\Database;
+
+// Auth
+use App\Modules\Auth\AuthRepository;
+use App\Modules\Auth\AuthService;
+use App\Modules\Auth\AuthController;
+
+class App
+{
+    private static ?self $instance = null;
+
+    private \PDO $pdo;
+
+    // Instances controllers
+    public AuthController $authController;
+
+    private function __construct()
+    {
+        $this->pdo = Database::getConnection();
+
+        // AUTH module
+        $authRepository = new AuthRepository($this->pdo);
+        $authService    = new AuthService($authRepository);
+        $this->authController = new AuthController($authService);
+    }
+
+    public static function getInstance(): self
+    {
+        return self::$instance ??= new self();
+    }
+}
