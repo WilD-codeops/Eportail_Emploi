bc89ee398c8fcc8638677fe67bef984d786210b2
WilD <wild.ajc1@gmail.com>
2025-12-06 11:53:07 +0100

    chore: Ajout routes pour Auth (login & inscription canddiat/entreprise)=

--- public/index.php
+++ public/index.php
@@ -11,4 +11,13 @@ $router = new Router();
 // Routes de test
 $router->get('/', 'App\\Modules\\Home\\HomeController@index');
 
+$router->get('/login', 'App\\Modules\\Auth\\AuthController@showLogin');
+$router->post('/login', 'App\\Modules\\Auth\\AuthController@login');
+
+$router->get('/register/candidat', 'App\\Modules\\Auth\\AuthController@showRegisterCandidat');
+$router->post('/register/candidat', 'App\\Modules\\Auth\\AuthController@registerCandidat');
+
+$router->get('/register/entreprise', 'App\\Modules\\Auth\\AuthController@showRegisterEntreprise');
+$router->post('/register/entreprise', 'App\\Modules\\Auth\\AuthController@registerEntreprise');
+
 $router->run();
