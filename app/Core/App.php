38d76b59c780793399218225deb94971c062fc12
WilD <wild.ajc1@gmail.com>
2025-12-07 11:55:14 +0100

    feat(core): enregistrer les dépendances du module entreprise dans App

--- app/Core/App.php
+++ app/Core/App.php
@@ -11,23 +11,42 @@ use App\Modules\Auth\AuthRepository;
 use App\Modules\Auth\AuthService;
 use App\Modules\Auth\AuthController;
 
+// Entreprise
+use App\Modules\Entreprise\EntrepriseRepository;
+use App\Modules\Entreprise\EntrepriseService;
+use App\Modules\Entreprise\EntrepriseController;
+
+
 class App
 {
     private static ?self $instance = null;
-
     private \PDO $pdo;
+    //Singleton et connexion PDO partagée
 
-    // Instances controllers
+
+    // Controllers à partager
     public AuthController $authController;
+    public EntrepriseController $entrepriseController;
 
     private function __construct()
     {
         $this->pdo = Database::getConnection();
 
-        // AUTH module
+        // Module Auth
         $authRepository = new AuthRepository($this->pdo);
-        $authService    = new AuthService($authRepository);
+        $authService    = new AuthService($authRepository, $this->pdo);
         $this->authController = new AuthController($authService);
+
+        // Module Entreprise
+        $entrepriseRepository = new EntrepriseRepository($this->pdo);
+        // besoin d'AuthService pour créer un gestionnaire lors de l'inscription
+        $entrepriseService = new EntrepriseService(
+            $entrepriseRepository,
+            $authService,
+            $this->pdo
+        );
+
+        $this->entrepriseController = new EntrepriseController($entrepriseService);
     }
 
     public static function getInstance(): self
@@ -37,10 +56,10 @@ class App
 
     public function resolve(string $class)
     {
-     return match ($class) 
-     {
-        \App\Modules\Auth\AuthController::class => $this->authController,
-        default => new $class(), // fallback si pas de dépendances (HomeController par ex)
+        return match ($class) {
+            \App\Modules\Auth\AuthController::class => $this->authController,
+            \App\Modules\Entreprise\EntrepriseController::class => $this->entrepriseController,
+            default => new $class(), // fallback si pas de dépendances (HomeController par ex)
         };
     }
 }
